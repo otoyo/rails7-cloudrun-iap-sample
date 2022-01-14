d9847885e44eafba5a75c30b614b3a04884f4778
otoyo <hiroki.t.1988@gmail.com>
2022-01-14 20:20:35 +0900

    Display IAP user email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+require "googleauth"
+
 class ApplicationController < ActionController::Base
+  def iap_user_email
+    iap_jwt = request.headers["X-Goog-Iap-Jwt-Assertion"]
+    aud = Rails.application.credentials.production[:iap_jwt_aud]
+    payload = Google::Auth::IDTokens.verify_iap iap_jwt, aud: aud
+    payload["email"]
+  end
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,5 +2,6 @@ class ArticlesController < ApplicationController
   layout "application"
 
   def index
+    @iap_user_email = iap_user_email
   end
 end
